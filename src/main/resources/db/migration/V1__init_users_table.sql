c2177b423b033ad0f21bc016ab745b766ca99a46
TheMadman <stretenskijdanila@gmail.com>
2024-02-16 21:37:53 +0300

    Add database

--- src/main/resources/db/migration/V1__init_users_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id serial NOT NULL constraint w_users_pk PRIMARY KEY,
-    telegram_id varchar(255),
-    client_id varchar(255),
-    client_secret varchar(255),
-    google_table_url varchar(255)
-);
-
